d9b25a0e4fad67323233fa1e0f8a6c6d62caac69
Kevin <kevin@Kevins-Air.home>
2017-05-27 15:58:33 -0500

    Created initial screens for landing page, 3D, 2D, and Video, as well as framework for hash pages

---
+++ scripts/global.js
@@ -0,0 +1,47 @@
+window.onload = function () {
+    var hash = window.location.hash;
+    if (hash) {
+        hash = hash.substr(1);
+        include(hash);
+    } else {
+        include();
+    }
+}
+
+function navigateToVideo(hash, path) {
+    window.location.hash = hash;
+    document.getElementById('content').setAttribute('data-include', path);
+    include(hash);
+}
+
+function include(hash) {
+	var elem = document.getElementById('content');
+
+    if (hash) {
+        var url = 'views/' + hash + '.html';
+    } else {
+        var url = 'views/navigation.html';
+    }
+	var localTest = /^(?:file):/,
+		xmlhttp = new XMLHttpRequest(),
+		status = 0;
+
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4) {
+			status = xmlhttp.status;
+		}
+		if (localTest.test(location.href) && xmlhttp.responseText) {
+			status = 200;
+		}
+		if (xmlhttp.readyState == 4 && status == 200) {
+			elem.outerHTML = xmlhttp.responseText;
+		}
+	}
+
+	try {
+		xmlhttp.open("GET", url, true);
+		xmlhttp.send();
+	} catch(err) {
+		/* todo catch error */
+	}
+}
